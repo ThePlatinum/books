6f0d1fac17fda2812a5d9f390280298d7a17bcb2
Platinum <platinumemirate@gmail.com>
2021-10-30 14:33:17 +0100

    Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import { Navbar, NavbarBrand, NavItem } from "reactstrap";
+
 export default class Header extends React.Component {
   constructor() {
-
+    super()
   }
 
   render() {
-    return <hi>Header </hi>
+    return (
+      <Navbar color='dark' dark className='header'>
+        <NavbarBrand href='/'> Books </NavbarBrand>
+      </Navbar>
+    )
   }
 }
